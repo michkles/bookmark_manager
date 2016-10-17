0887b90fb82caa19d7a4ddbff97c44dd257db75c
michkles <michkles@gmail.com>
2016-10-17 16:22:49 +0100

    first commit

---
+++ app/models/link.rb
@@ -0,0 +1,18 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+
+class Link
+  include DataMapper::Resource
+
+  property :id,     Serial # Serial means that it will be auto-incremented for every record
+  property :title,  String
+  property :url,    String
+
+end
+
+# Now let's set up a connection with a database
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test")
+# Let's check that everything we wrote in our models was OK
+DataMapper.finalize
+# And let's build any new columns or tables we added
+DataMapper.auto_upgrade!
