93511e61e92c4c798122d31e35d5b58be891ccc5
michkles <michkles@gmail.com>
2016-10-18 11:33:00 +0100

    step13 added rack env

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,7 @@
+ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
 require_relative 'models/link'
 require 'database_cleaner'
-
 class BookmarkManager < Sinatra::Base
   get '/links' do
     @links = Link.all
