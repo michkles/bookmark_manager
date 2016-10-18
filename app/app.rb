e55a7ebde0adead2a3ed63d3b1398a5314967810
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-18 13:21:42 +0100

    Removed database_cleaner requirement from app.rb

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,8 @@
 ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
 require_relative 'models/link'
-require 'database_cleaner'
 class BookmarkManager < Sinatra::Base
-  
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
