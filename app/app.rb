29d970d9319701a53e0e5f7a771be3da099d4128
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-18 12:32:08 +0100

    Updated link.rb with specific reference to heroku db instance

--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,7 @@ require 'sinatra/base'
 require_relative 'models/link'
 require 'database_cleaner'
 class BookmarkManager < Sinatra::Base
+  
   get '/links' do
     @links = Link.all
     erb :'links/index'
