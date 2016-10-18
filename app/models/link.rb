e60ece6fc6fa63b4308726be2bc669ac526269b4
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-18 12:46:31 +0100

    Made some tweaks to gemfile, removed specific DB URL from link.rb

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,7 +10,7 @@ class Link
 
 end
 # Now let's set up a connection with a database
-DataMapper.setup(:default, ENV['postgres://keawipgiereusg:GbPWb2YsMPLcw2ROpii4HklF1j@ec2-184-73-202-229.compute-1.amazonaws.com:5432/d8688odvu532hm'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 # Let's check that everything we wrote in our models was OK
 DataMapper.finalize
 # And let's build any new columns or tables we added
