2e768b0c0f060311675004222b8f9677394da992
hanrattyjen <hanratty.jen@gmail.com>
2016-10-19 12:55:04 +0100

    Added multiple tags functionality.

--- app/app.rb
+++ app/app.rb
@@ -23,11 +23,12 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.new(url: params[:url], title: params[:title])
-    tag = Tag.first_or_create(name: params[:tags])
-    link.tags << tag
+    link = Link.create(url: params[:url], title: params[:title])
+    params[:tags].split.each do |tag|
+      link.tags << Tag.create(name: tag)
+    end
     link.save
-    redirect '/links'
+    redirect to('/links')
   end
 
   get '/tags/:name' do
--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -12,3 +12,17 @@ feature 'I want to add a single tag to a link' do
     expect(link.tags.map(&:name)).to include('zombo')
   end
 end
+
+feature 'I want to add multiple tags' do
+  scenario 'user can add more than one tag' do
+    visit('/links/new')
+    fill_in 'title', with: 'This is ZomboCom'
+    fill_in 'url', with: 'http://www.zombo.com'
+    fill_in 'tags', with: 'zombo website singing'
+    click_button('Submit')
+
+
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('website', 'zombo')
+  end
+end
