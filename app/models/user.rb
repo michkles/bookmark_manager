cd749064437a6c8b37449d7e1791d167e97edf1d
michkles <michkles@gmail.com>
2016-10-20 15:00:27 +0100

    step21 added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,19 @@ class User
   attr_reader   :password
   attr_accessor :password_confirmation
   validates_confirmation_of :password
+  validates_format_of :email, as: :email_address
 
   property :id, Serial
-  property :email, String
+  property :email, String, required: true
   property :password_digest, Text
 
-  validates_confirmation_of :password
 
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
+
+
+
+
 end
--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -21,5 +21,30 @@ feature 'User sign in' do
     expect(page).to have_content 'Password and confirmation password do not match'
   end
 
+  scenario 'if email is not filled in ' do
+    def no_email
+      visit '/users/new'
+      expect(page.status_code).to eq 200
+      fill_in :email, with: ''
+      fill_in :password, with: 'password'
+      fill_in :password_confirmation, with: 'password'
+      click_button 'Register'
+    end
+    expect { no_email }.not_to change(User, :count)
+  end
+
+
+  scenario 'if email is not valid' do
+    def invalid_email
+      visit '/users/new'
+      expect(page.status_code).to eq 200
+      fill_in :email, with: 'blabla@bla'
+      fill_in :password, with: 'password'
+      fill_in :password_confirmation, with: 'password'
+      click_button 'Register'
+    end
+    expect { invalid_email }.not_to change(User, :count)
+  end
+
 
 end
